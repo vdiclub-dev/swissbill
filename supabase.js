28281a2edad63c8b9f80c8539c78483b2472432e
vdiclub-dev <vdiclub@gmail.com>
2026-03-05 12:28:09 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,12 @@
+// supabase.js
 const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co"
-const SUPABASE_KEY = "sb_publishable_AkBuF7AiMVxXuvTF1Mx3Zw_8AuW3gGb"
+const SUPABASE_ANON_KEY = "sb_publishable_AkBuF7AiMVxXuvTF1Mx3Zw_8AuW3gGb"
 
-const { createClient } = supabase
-
-const supabaseClient = createClient(
+// La librairie expose window.supabase
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_KEY
-)
+  SUPABASE_ANON_KEY
+);
+
+// Optionnel : rendre accessible partout (si d'autres fichiers en ont besoin)
+window.supabaseClient = supabaseClient;
