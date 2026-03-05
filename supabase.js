0f282ce2ce2f24fa86ead18be97c7a34781bd24f
vdiclub-dev <vdiclub@gmail.com>
2026-03-05 18:21:38 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,22 +1,19 @@
-// supabase.js
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+
+<script>
 
 const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co";
 
 const SUPABASE_ANON_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y";
 
-// Vérifier que la librairie Supabase est chargée
-if (window.supabase) {
+const { createClient } = supabase;
 
-  window.supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
+window.supabaseClient = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
-  console.log("Supabase connecté ✅");
+console.log("Supabase connecté", window.supabaseClient);
 
-} else {
-
-  console.error("Supabase JS non chargé ❌");
-
-}
+</script>
