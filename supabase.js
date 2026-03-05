b7632f5206a3820c8b2db3b1c5d3d480ae81a79a
vdiclub-dev <vdiclub@gmail.com>
2026-03-05 16:44:29 +0100

    Change SUPABASE_ANON_KEY to new value
    Updated the SUPABASE_ANON_KEY to a new value.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co"
-const SUPABASE_ANON_KEY = "sb_publishable_AkBuF7AiMVxXuvTF1Mx3Zw_8AuW3gGb"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y"
 
 // La librairie expose window.supabase
 const supabaseClient = window.supabase.createClient(
