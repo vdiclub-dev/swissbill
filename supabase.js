06757f545b9f22d098474673af6ce705c7f11f0f
vdiclub-dev <vdiclub@gmail.com>
2026-03-05 18:13:05 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,22 @@
-const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co"
+// supabase.js
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y"
+const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co";
 
-window.supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-)
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y";
+
+// Vérifier que la librairie Supabase est chargée
+if (window.supabase) {
+
+  window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+
+  console.log("Supabase connecté ✅");
+
+} else {
+
+  console.error("Supabase JS non chargé ❌");
+
+}
