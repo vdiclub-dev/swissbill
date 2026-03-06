676c1ff23d29905be26005fba8b27cca8f1b695a
vdiclub-dev <vdiclub@gmail.com>
2026-03-06 17:48:25 +0100

    Update clients.js

--- js/clients.js
+++ js/clients.js
@@ -3,16 +3,8 @@ async function loadClients(){
 const { data, error } = await db
 .from("clients")
 .select("*")
-.order("id",{ascending:false})
-
-if(error){
-console.log("Erreur clients:",error)
-return
-}
 
 const tbody = document.getElementById("clientsTable")
-
-if(!tbody) return
 
 tbody.innerHTML=""
 
@@ -20,9 +12,9 @@ data.forEach(c=>{
 
 tbody.innerHTML += `
 <tr>
-<td>${c.company || ""}</td>
-<td>${c.last_name || ""}</td>
-<td>${c.email || ""}</td>
+<td>${c.company}</td>
+<td>${c.last_name}</td>
+<td>${c.email}</td>
 </tr>
 `
 
