bcaa4123040391c4ca674d00dcfda382bdb24890
vdiclub-dev <vdiclub@gmail.com>
2026-03-07 18:43:24 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -21,3 +21,34 @@ return
 window.location.href = "portal.html"
 
 }
+async function login(){
+
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const {data,error} = await db.auth.signInWithPassword({
+email:email,
+password:password
+})
+
+if(error){
+alert("Email ou mot de passe incorrect")
+return
+}
+
+window.location.href="portal.html"
+
+}
+async function checkLogin(){
+
+const {data} = await db.auth.getSession()
+
+if(!data.session){
+
+window.location.href="login.html"
+
+}
+
+}
+
+document.addEventListener("DOMContentLoaded",checkLogin)
