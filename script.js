da3e8eb815d0edccccf61271d05fe3a1767d71e3
vdiclub-dev <vdiclub@gmail.com>
2026-03-07 14:11:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let map
 
+function initMap(){
+
+map = L.map('map').setView([46.8,7.2],8)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+maxZoom:19
+}).addTo(map)
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+initMap()
+
+})
