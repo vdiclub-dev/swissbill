518659b4ae4e691c5e6cb778a59b635f6359fc46
vdiclub-dev <vdiclub@gmail.com>
2026-03-07 18:38:22 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,1 +1,23 @@
+async function login(){
 
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const {data,error} = await db.auth.signInWithPassword({
+
+email:email,
+password:password
+
+})
+
+if(error){
+
+alert("Erreur connexion")
+
+return
+
+}
+
+window.location.href = "portal.html"
+
+}
