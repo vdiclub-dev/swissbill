fb6650adeaa1b5b04065d6fd8f6adeb0286dd718
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 00:12:43 +0100

    Update routes.js

--- JS/routes.js
+++ JS/routes.js
@@ -1,1 +1,38 @@
+const sb = window.supabaseClient
 
+async function loadRoutes(){
+
+const today =
+new Date().toISOString().slice(0,10)
+
+const {data} = await sb
+.from("orders")
+.select("*")
+.gte("created_at", today)
+
+const table =
+document.getElementById("routesTable")
+
+table.innerHTML=""
+
+data.forEach(o=>{
+
+table.innerHTML += `
+<tr>
+
+<td>${o.order_number}</td>
+
+<td>${o.pickup_city}</td>
+
+<td>${o.delivery_city}</td>
+
+<td>${o.status}</td>
+
+</tr>
+`
+
+})
+
+}
+
+loadRoutes()
