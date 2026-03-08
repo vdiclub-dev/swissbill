d375798cf009ba2268ab00c733e1e8a74d545188
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 18:57:17 +0100

    Update admin.js

--- JS/admin.js
+++ JS/admin.js
@@ -1,1 +1,55 @@
+const sb = window.supabaseClient
 
+async function loadOrders(){
+
+const {data} = await sb
+.from("orders")
+.select("*")
+.order("created_at",{ascending:false})
+
+const table = document.getElementById("ordersTable")
+
+table.innerHTML=""
+
+data.forEach(order=>{
+
+table.innerHTML += `
+<tr>
+
+<td>${order.order_number}</td>
+
+<td>${order.pickup_city}</td>
+
+<td>${order.delivery_city}</td>
+
+<td>${order.status}</td>
+
+<td>
+<button onclick="setStatus('${order.id}','en_route')">
+En route
+</button>
+
+<button onclick="setStatus('${order.id}','livre')">
+Livré
+</button>
+</td>
+
+</tr>
+`
+
+})
+
+}
+
+async function setStatus(id,status){
+
+await sb
+.from("orders")
+.update({status:status})
+.eq("id",id)
+
+loadOrders()
+
+}
+
+loadOrders()
