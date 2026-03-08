711b5cf8a14faa694607f9a13c4669521a11a0d2
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 17:21:00 +0100

    Update invoice.js

--- JS/invoice.js
+++ JS/invoice.js
@@ -3,12 +3,11 @@ function generateInvoice(order){
 const doc = new jspdf.jsPDF()
 
 doc.text("Léman-Courses",20,20)
-
 doc.text("Facture transport",20,40)
 
 doc.text("Commande : "+order.order_number,20,60)
 
-doc.text("Prix : CHF "+order.price,20,80)
+doc.text("Montant : CHF "+order.price,20,80)
 
 doc.save("facture_"+order.order_number+".pdf")
 
