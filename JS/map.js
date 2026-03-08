cdabaafaa787ccc47cbf36db7b555640ab85af82
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 16:46:32 +0100

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -1,1 +1,29 @@
 
+async function getDistance(start, end){
+
+const key = "TA_CLE_OPENROUTESERVICE"
+
+const url = `
+https://api.openrouteservice.org/v2/directions/driving-car
+`
+
+const response = await fetch(url,{
+method:"POST",
+headers:{
+"Authorization":key,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+coordinates:[
+[start.lng,start.lat],
+[end.lng,end.lat]
+]
+})
+})
+
+const data = await response.json()
+
+const km = data.routes[0].summary.distance / 1000
+
+return km
+}
