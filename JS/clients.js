d1d269f342c3df4435bbe8ca71f9f587c18160e1
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 19:28:28 +0100

    Update clients.js

--- JS/clients.js
+++ JS/clients.js
@@ -1,1 +1,52 @@
+const sb = window.supabaseClient
 
+async function loadClients(){
+
+const {data} = await sb
+.from("clients")
+.select("*")
+.order("nom")
+
+const table = document.getElementById("clientsTable")
+
+table.innerHTML=""
+
+data.forEach(c=>{
+
+table.innerHTML += `
+<tr>
+
+<td>${c.nom}</td>
+<td>${c.entreprise||""}</td>
+<td>${c.email||""}</td>
+<td>${c.telephone||""}</td>
+
+</tr>
+`
+
+})
+
+}
+
+document
+.getElementById("clientForm")
+.addEventListener("submit",async e=>{
+
+e.preventDefault()
+
+await sb.from("clients").insert({
+
+nom:nom.value,
+entreprise:entreprise.value,
+email:email.value,
+telephone:telephone.value,
+adresse:adresse.value,
+ville:ville.value
+
+})
+
+loadClients()
+
+})
+
+loadClients()
