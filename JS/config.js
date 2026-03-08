48878dfa890b59f3657fbfc22e70ca123d47c8ec
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 16:23:56 +0100

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -1,7 +1,7 @@
+window.SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y";
 
-const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y"
-
-window.db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
-
-console.log("Supabase connecté")
+window.supabaseClient = supabase.createClient(
+  window.SUPABASE_URL,
+  window.SUPABASE_ANON_KEY
+);
