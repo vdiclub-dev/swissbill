a1b1df794399e53ddc06535ebb79a018ce709074
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 06:20:08 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,53 @@
-async function login(){
+async function signup(){
+
+const company=document.getElementById("company").value
+const lastname=document.getElementById("lastname").value
+const firstname=document.getElementById("firstname").value
+const phone=document.getElementById("phone").value
+const address=document.getElementById("address").value
+const zip=document.getElementById("zip").value
+const city=document.getElementById("city").value
 
 const email=document.getElementById("email").value
 const password=document.getElementById("password").value
 
-const {data,error}=await db.auth.signInWithPassword({
-email,
-password
+const { data, error } = await db.auth.signUp({
+email:email,
+password:password
+})
+
+if(error){
+alert(error.message)
+return
+}
+
+await db.from("clients").insert([{
+auth_id:data.user.id,
+company,
+lastname,
+firstname,
+phone,
+address,
+zip,
+city,
+email
+}])
+
+alert("Compte créé")
+
+window.location.href="login.html"
+
+}
+
+
+async function login(){
+
+const email=document.getElementById("loginEmail").value
+const password=document.getElementById("loginPassword").value
+
+const { data, error } = await db.auth.signInWithPassword({
+email:email,
+password:password
 })
 
 if(error){
@@ -14,26 +56,5 @@ return
 }
 
 window.location.href="portal.html"
-
-}
-
-async function signup(){
-
-const email=document.getElementById("email").value
-const password=document.getElementById("password").value
-
-const {data,error}=await db.auth.signUp({
-email,
-password
-})
-
-if(error){
-alert(error.message)
-return
-}
-
-alert("Compte créé")
-
-window.location.href="login.html"
 
 }
