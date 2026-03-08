0792a1a37b777f655274d83d070ee292ef712328
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 11:33:42 +0100

    Update orders.js

--- JS/orders.js
+++ JS/orders.js
@@ -1,43 +1,19 @@
 async function createOrder(){
 
-const { data: { user } } = await db.auth.getUser()
+const client = document.getElementById("Client").value
+const pickup = document.getElementById("pickup_address").value
+const delivery = document.getElementById("delivery_address").value
+const distance = document.getElementById("Distance").value
+const prix = document.getElementById("Prix").value
 
-const pickup_company = document.getElementById("pickup_company").value
-const pickup_address = document.getElementById("pickup_address").value
-const pickup_zip = document.getElementById("pickup_zip").value
-const pickup_city = document.getElementById("pickup_city").value
+await db.from("transport_orders").insert([{
 
-const delivery_company = document.getElementById("delivery_company").value
-const delivery_address = document.getElementById("delivery_address").value
-const delivery_zip = document.getElementById("delivery_zip").value
-const delivery_city = document.getElementById("delivery_city").value
-
-const colis = document.getElementById("colis").value
-const weight = document.getElementById("weight").value
-
-const speed = document.getElementById("speed").value
-
-await db
-.from("orders")
-.insert([{
-
-user_id:user.id,
-
-pickup_company,
-pickup_address,
-pickup_zip,
-pickup_city,
-
-delivery_company,
-delivery_address,
-delivery_zip,
-delivery_city,
-
-colis,
-weight,
-speed,
-
-status:"nouveau"
+Client:client,
+pickup_address:pickup,
+delivery_address:delivery,
+Distance:distance,
+Prix:prix,
+Statut:"nouveau"
 
 }])
 
