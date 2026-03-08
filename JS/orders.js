81da3a8bd75e7277b8f0fd883881730062ae3ba1
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 11:04:33 +0100

    Update orders.js

--- JS/orders.js
+++ JS/orders.js
@@ -1,1 +1,21 @@
+async function createOrder(){
 
+const { data: { user } } = await db.auth.getUser()
+
+const pickup = document.getElementById("pickup").value
+const delivery = document.getElementById("delivery").value
+const weight = document.getElementById("weight").value
+
+await db
+.from("orders")
+.insert([{
+user_id:user.id,
+pickup,
+delivery,
+weight,
+status:"nouveau"
+}])
+
+loadOrders()
+
+}
