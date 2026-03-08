9583685386bba8fb00b755daf366a9c55c211a4c
vdiclub-dev <vdiclub@gmail.com>
2026-03-08 01:16:59 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,62 +1,11 @@
-let map
-let stops=[]
-
-function initMap(){
-
-if(!document.getElementById("map")) return
-
-map = L.map('map').setView([46.8,7.2],8)
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-maxZoom:19
-}).addTo(map)
-
-}
-
-
-
-
-
-async function sendParcel(){
-
-const payload={
-
-client:document.getElementById("client").value,
-ville:document.getElementById("ville").value,
-adresse:document.getElementById("adresse").value,
-
-colis:Number(document.getElementById("colis").value),
-poids:Number(document.getElementById("poids").value),
-
-delai:document.getElementById("delai").value,
-nuit:document.getElementById("nuit").value,
-note:document.getElementById("note").value
-
-}
-
-const {error}=await db.from("colis").insert([payload])
-
-if(error){
-
-alert("Erreur : "+error.message)
-
-}else{
-
-alert("Demande envoyée")
-
-loadColis()
-
-}
-
-}
 async function login(){
 
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
 
-const {data,error} = await db.auth.signInWithPassword({
-email:email,
-password:password
+const {data,error}=await db.auth.signInWithPassword({
+email,
+password
 })
 
 if(error){
@@ -65,5 +14,26 @@ return
 }
 
 window.location.href="portal.html"
+
+}
+
+async function signup(){
+
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
+
+const {data,error}=await db.auth.signUp({
+email,
+password
+})
+
+if(error){
+alert(error.message)
+return
+}
+
+alert("Compte créé")
+
+window.location.href="login.html"
 
 }
