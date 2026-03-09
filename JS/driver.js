b642cc87d56f057a040e120ffff5bcdddd392914
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 18:05:21 +0100

    Update driver.js

--- JS/driver.js
+++ JS/driver.js
@@ -6,3 +6,31 @@ await supabaseClient
 .eq("id",id)
 
 }
+navigator.geolocation.watchPosition(pos=>{
+
+const lat = pos.coords.latitude
+const lon = pos.coords.longitude
+
+supabaseClient
+.from("vehicles")
+.update({
+lat:lat,
+lon:lon,
+updated_at:new Date()
+})
+.eq("driver_id",driverId)
+
+})
+async function startPause(){
+
+await supabaseClient
+.from("driver_logs")
+.insert({
+
+driver_id:driverId,
+type:"pause",
+time:new Date()
+
+})
+
+}
