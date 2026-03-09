d3ff1e6f5abd836e3dab9f5eda2f0e0916c174fa
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 10:50:14 +0100

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -1,1 +1,21 @@
 console.log("map.js chargé")
+let routeLayer = null;
+
+function drawRoute(coords){
+
+if(routeLayer){
+map.removeLayer(routeLayer)
+}
+
+const latlngs = coords.map(c => [c[1], c[0]])
+
+routeLayer = L.polyline(latlngs,{
+color:"red",
+weight:4
+}).addTo(map)
+
+map.fitBounds(routeLayer.getBounds())
+
+}
+drawRoute(data.features[0].geometry.coordinates)
+const minutes = data.features[0].properties.summary.duration / 60
