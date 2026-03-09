6afb4477885afa7a12e32d086c388ec5c9110ff2
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 11:01:28 +0100

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -1,10 +1,14 @@
 console.log("map.js chargé")
 
-document.addEventListener("DOMContentLoaded", function(){
+let map
 
-if(!document.getElementById("map")) return
+document.addEventListener("DOMContentLoaded", () => {
 
-const map = L.map("map").setView([46.8,6.6],8)
+const mapDiv = document.getElementById("map")
+
+if(!mapDiv) return
+
+map = L.map("map").setView([46.8,6.6],8)
 
 L.tileLayer(
 "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
