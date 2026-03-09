f5f1c5662fe0047b423868ec9cbb20d7bff5e2ce
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 17:44:28 +0100

    Update cockpit.js

--- JS/cockpit.js
+++ JS/cockpit.js
@@ -1,1 +1,33 @@
+console.log("cockpit chargé")
 
+let map
+
+document.addEventListener("DOMContentLoaded",init)
+
+function init(){
+
+map = L.map("map").setView([46.8,6.6],8)
+
+L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+).addTo(map)
+
+loadDrivers()
+
+}
+
+async function loadDrivers(){
+
+const {data} = await supabaseClient
+.from("vehicles")
+.select("*")
+
+data.forEach(v=>{
+
+L.marker([v.lat,v.lon])
+.addTo(map)
+.bindPopup("Chauffeur")
+
+})
+
+}
