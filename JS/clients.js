8cd61f2bc842901f169b7665637b9b330052d03b
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 09:04:24 +0100

    Update clients.js

--- JS/clients.js
+++ JS/clients.js
@@ -1,7 +1,15 @@
-console.log("clients.js chargé")
+console.log("clients.js chargé");
 
-function searchClient(){
-console.log("recherche client")
+function searchClient() {
+  const term = document.getElementById("clientSearch").value.trim();
+  const results = document.getElementById("clientResults");
+
+  if (!term) {
+    results.innerHTML = "";
+    return;
+  }
+
+  results.innerHTML = "<div>Recherche : " + term + "</div>";
 }
 
-window.searchClient = searchClient
+window.searchClient = searchClient;
