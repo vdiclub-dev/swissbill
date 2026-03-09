c30cdaf5cb941d9c6b46c520334b94191c15d754
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 07:18:28 +0100

    Update orders.js

--- JS/orders.js
+++ JS/orders.js
@@ -1,25 +1,3 @@
-function calculatePrice(){
-
-let km =
-Number(document.getElementById("distance").innerText)
-
-let packageType =
-document.getElementById("package_type").value
-
-let price = km * 1.20
-
-if(packageType === "palette"){
-price += 20
-}
-
-if(packageType === "box"){
-price += 10
-}
-
-document.getElementById("price").innerText =
-"CHF " + price.toFixed(2)
-
-}
 async function calculateDistance(){
 
 let start = document.getElementById("pickup_address").value
@@ -27,14 +5,16 @@ let end = document.getElementById("delivery_address").value
 
 if(!start || !end) return
 
-const apiKey = "TA_CLE_OPENROUTE"
+const geo = async (address) => {
 
-const geo = async (addr)=>{
 const res = await fetch(
-`https://api.openrouteservice.org/geocode/search?api_key=${eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImI4OTQwOGJlOTE1MDQzNjc5NmQ3NzkzOWQ0YjZjODg4IiwiaCI6Im11cm11cjY0In0=}&text=${encodeURIComponent(addr)}`
+`https://api.openrouteservice.org/geocode/search?api_key=${eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImI4OTQwOGJlOTE1MDQzNjc5NmQ3NzkzOWQ0YjZjODg4IiwiaCI6Im11cm11cjY0In0=}&text=${encodeURIComponent(address)}`
 )
+
 const data = await res.json()
+
 return data.features[0].geometry.coordinates
+
 }
 
 const startCoord = await geo(start)
@@ -45,7 +25,7 @@ const routeRes = await fetch(
 {
 method:"POST",
 headers:{
-"Authorization":apiKey,
+"Authorization":ORS_API_KEY,
 "Content-Type":"application/json"
 },
 body:JSON.stringify({
@@ -57,7 +37,7 @@ coordinates:[startCoord,endCoord]
 const routeData = await routeRes.json()
 
 const meters = routeData.routes[0].summary.distance
-const km = meters/1000
+const km = meters / 1000
 
 document.getElementById("distance").innerText =
 km.toFixed(1)
