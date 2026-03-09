dbeada94e20521a067217bafa517bb5e8881b145
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 09:00:32 +0100

    Update orders.js

--- JS/orders.js
+++ JS/orders.js
@@ -1,15 +1,46 @@
 console.log("orders.js chargé")
 
 function calculateDistance(){
-console.log("distance")
+
+const start =
+document.getElementById("pickup_address").value
+
+const end =
+document.getElementById("delivery_address").value
+
+if(!start || !end){
+alert("Entrer les deux adresses")
+return
+}
+
+const km = 25
+
+document.getElementById("distance").innerText = km
+
+calculatePrice()
+
 }
 
 function calculatePrice(){
-console.log("prix")
+
+const km =
+Number(document.getElementById("distance").innerText)
+
+const type =
+document.getElementById("package_type").value
+
+let price = km * 1.2
+
+if(type==="palette") price += 20
+if(type==="box") price += 10
+
+document.getElementById("price").innerText =
+"CHF " + price.toFixed(2)
+
 }
 
 function calculateTransport(){
-console.log("transport")
+calculateDistance()
 }
 
 window.calculateDistance = calculateDistance
