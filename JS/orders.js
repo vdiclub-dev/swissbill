2cd1cb822393093cf25f92abbf5b5b37e3b6f320
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 06:59:33 +0100

    Update orders.js

--- JS/orders.js
+++ JS/orders.js
@@ -1,40 +1,46 @@
-function calculateDistance(){
+async function calculateDistance(){
 
 let start = document.getElementById("pickup_address").value
 let end = document.getElementById("delivery_address").value
 
 if(!start || !end) return
 
-// simulation distance
-let km = Math.floor(Math.random()*80)+10
+const apiKey = "TA_CLE_API"
 
-document.getElementById("distance").innerText = km
+const geoUrl = (addr)=>
+`https://api.openrouteservice.org/geocode/search?api_key=${eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImI4OTQwOGJlOTE1MDQzNjc5NmQ3NzkzOWQ0YjZjODg4IiwiaCI6Im11cm11cjY0In0=}&text=${encodeURIComponent(addr)}`
+
+const startRes = await fetch(geoUrl(start))
+const startData = await startRes.json()
+
+const endRes = await fetch(geoUrl(end))
+const endData = await endRes.json()
+
+const startCoord = startData.features[0].geometry.coordinates
+const endCoord = endData.features[0].geometry.coordinates
+
+const routeRes = await fetch(
+"https://api.openrouteservice.org/v2/directions/driving-car",
+{
+method:"POST",
+headers:{
+"Authorization":apiKey,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+coordinates:[startCoord,endCoord]
+})
+})
+
+const routeData = await routeRes.json()
+
+const meters = routeData.routes[0].summary.distance
+
+const km = meters/1000
+
+document.getElementById("distance").innerText =
+km.toFixed(1)
 
 calculatePrice()
-
-}
-
-
-function calculateTransport(){
-
-calculateDistance()
-
-}
-
-
-function calculatePrice(){
-
-let km = Number(document.getElementById("distance").innerText)
-
-let packageType = document.getElementById("package_type").value
-
-let price = km * 1.20
-
-if(packageType === "palette"){
-price += 20
-}
-
-document.getElementById("price").innerText =
-"CHF " + price.toFixed(2)
 
 }
