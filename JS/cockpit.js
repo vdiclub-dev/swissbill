212c8985255f5d7be1635d12d27e8a34c7ecf0d9
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 17:47:58 +0100

    Update cockpit.js

--- JS/cockpit.js
+++ JS/cockpit.js
@@ -31,3 +31,49 @@ L.marker([v.lat,v.lon])
 })
 
 }
+async function loadOrdersToday(){
+
+const today = new Date().toISOString().split("T")[0]
+
+const {data} = await supabaseClient
+.from("orders")
+.select("*")
+.eq("date",today)
+
+return data
+
+}
+function optimizeRoute(start,orders){
+
+let route = []
+let current = start
+
+while(orders.length){
+
+let nearest = orders[0]
+let minDist = distance(current,nearest)
+
+orders.forEach(o=>{
+
+const d = distance(current,o)
+
+if(d < minDist){
+
+nearest = o
+minDist = d
+
+}
+
+})
+
+route.push(nearest)
+
+current = nearest
+
+orders = orders.filter(o=>o.id!==nearest.id)
+
+}
+
+return route
+
+}
