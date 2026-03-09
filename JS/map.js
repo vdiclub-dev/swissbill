b2dfb832eacf2573702c518e43f28d9310c884c8
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 08:04:29 +0100

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -1,81 +1,29 @@
-async function calculateRoute(start, end) {
+console.log("maps.js chargé")
 
-const key = "CLE_OPENROUTE"
-
-const url =
-"https://api.openrouteservice.org/v2/directions/driving-car"
-
-const response = await fetch(url,{
-method:"POST",
-headers:{
-"Authorization":key,
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-coordinates:[
-[start.lng,start.lat],
-[end.lng,end.lat]
-]
-})
-})
-
-const data = await response.json()
-
-const km = data.routes[0].summary.distance / 1000
-const duration = data.routes[0].summary.duration / 60
-
-return {km,duration}
-}
+// centre Suisse
 const map = L.map('map').setView([46.8, 6.6], 8)
 
+// fond de carte OpenStreetMap
 L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
-maxZoom:19
+maxZoom: 19,
+attribution: '© OpenStreetMap'
 }).addTo(map)
-async function calculateDistance(){
 
-const start =
-document.getElementById("pickup_address").value
+let markerStart = null
+let markerEnd = null
+function showPoints(lat1, lon1, lat2, lon2){
 
-const end =
-document.getElementById("delivery_address").value
+if(markerStart) map.removeLayer(markerStart)
+if(markerEnd) map.removeLayer(markerEnd)
 
-if(!start || !end) return
+markerStart = L.marker([lat1, lon1]).addTo(map)
+markerEnd = L.marker([lat2, lon2]).addTo(map)
 
-const url =
-`https://nominatim.openstreetmap.org/search?format=json&q=${start}`
-
-const res = await fetch(url)
-
-const data = await res.json()
-
-if(!data[0]) return
-
-const lat1 = data[0].lat
-const lon1 = data[0].lon
-
-const url2 =
-`https://nominatim.openstreetmap.org/search?format=json&q=${end}`
-
-const res2 = await fetch(url2)
-
-const data2 = await res2.json()
-
-if(!data2[0]) return
-
-const lat2 = data2[0].lat
-const lon2 = data2[0].lon
-
-const km =
-Math.sqrt(
-Math.pow(lat2-lat1,2)+
-Math.pow(lon2-lon1,2)
-)*111
-
-document.getElementById("distance")
-.innerText = km.toFixed(1)
-
-calculatePrice()
+map.fitBounds([
+[lat1, lon1],
+[lat2, lon2]
+])
 
 }
