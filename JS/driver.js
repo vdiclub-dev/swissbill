e0482c9cdc5f68610bfd677d8d96304f7ec69478
vdiclub-dev <vdiclub@gmail.com>
2026-03-09 18:24:31 +0100

    Update driver.js

--- JS/driver.js
+++ JS/driver.js
@@ -34,3 +34,65 @@ time:new Date()
 })
 
 }
+console.log("driver chargé")
+
+let map
+
+document.addEventListener("DOMContentLoaded",init)
+
+function init(){
+
+map = L.map("map").setView([46.8,6.6],8)
+
+L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+).addTo(map)
+
+loadDriverOrders()
+
+}
+function startScanner(){
+
+const scanner = new Html5Qrcode("qr-reader")
+
+scanner.start(
+{ facingMode:"environment" },
+
+{ fps:10, qrbox:250 },
+
+(code)=>{
+
+console.log("QR code :",code)
+
+openOrder(code)
+
+}
+
+)
+
+}
+navigator.geolocation.watchPosition(pos=>{
+
+const lat = pos.coords.latitude
+const lon = pos.coords.longitude
+
+supabaseClient
+.from("vehicles")
+.update({
+lat:lat,
+lon:lon
+})
+
+})
+async function startPause(){
+
+await supabaseClient
+.from("driver_logs")
+.insert({
+
+type:"pause",
+time:new Date()
+
+})
+
+}
