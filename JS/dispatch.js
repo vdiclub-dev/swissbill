dd2d97d5467a2f020dbc3dadd9be7f1c9554ec17
vdiclub-dev <vdiclub@gmail.com>
2026-03-10 23:53:33 +0100

    Update dispatch.js

--- JS/dispatch.js
+++ JS/dispatch.js
@@ -1,20 +1,4 @@
-console.log("dispatch chargé")
 
-/* ---------------------- */
-/* CARTE DISPATCH */
-/* ---------------------- */
-
-const map = L.map('map').setView([46.52,6.63],9)
-
-L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-maxZoom:18
-}).addTo(map)
-
-L.marker([46.5197,6.6323])
-.addTo(map)
-.bindPopup("Test Lausanne")
 
 
 /* ---------------------- */
