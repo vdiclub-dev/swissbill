b80c213d4989a751f4b92cb9ec1e117fe032ebbf
vdiclub-dev <vdiclub@gmail.com>
2026-03-10 18:11:39 +0100

    Update orders.js

--- JS/orders.js
+++ JS/orders.js
@@ -1,1 +1,21 @@
+async function createOrder(data){
 
+const { error } = await supabase
+.from("orders")
+.insert([
+{
+client_id: data.client_id,
+pickup_address: data.pickup,
+delivery_address: data.delivery,
+product_type: data.type,
+weight: data.weight,
+status: "pending"
+}
+])
+
+if(error){
+alert("Erreur création transport")
+}else{
+alert("Transport enregistré")
+}
+}
