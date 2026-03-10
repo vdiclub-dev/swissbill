8ead2bdffed94352e28f39034eed491af5ec0bc3
vdiclub-dev <vdiclub@gmail.com>
2026-03-10 18:18:42 +0100

    Update dispatch.js

--- JS/dispatch.js
+++ JS/dispatch.js
@@ -1,1 +1,36 @@
+async function loadOrders(){
 
+const { data, error } = await supabase
+.from("orders")
+.select("*")
+.eq("status","pending")
+
+const table = document.querySelector("#ordersTable tbody")
+
+table.innerHTML = ""
+
+data.forEach(order=>{
+
+const row = document.createElement("tr")
+
+row.innerHTML = `
+
+<td>${order.id}</td>
+<td>${order.delivery_city}</td>
+<td>${order.service}</td>
+
+<td>
+<button class="btn" onclick="planOrder('${order.id}')">
+Planifier
+</button>
+</td>
+
+`
+
+table.appendChild(row)
+
+})
+
+}
+
+loadOrders()
