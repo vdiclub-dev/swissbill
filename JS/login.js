673647c123b159192e7bb9a10098989b85891b34
vdiclub-dev <vdiclub@gmail.com>
2026-03-10 17:46:29 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,18 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+async function login(){
 
-<script src="js/config.js"></script>
-<script src="js/login.js"></script>
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
+
+const {data,error}=await db.auth.signInWithPassword({
+email:email,
+password:password
+})
+
+if(error){
+alert("Email ou mot de passe incorrect")
+return
+}
+
+window.location.href="portal.html"
+
+}
