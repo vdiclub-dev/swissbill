d61ffc9f6aa8d7c301041d6e0aa24d5a2dff502f
vdiclub-dev <vdiclub@gmail.com>
2026-03-10 17:45:53 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,40 +1,4 @@
-console.log("login.js chargé")
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
-document.addEventListener("DOMContentLoaded", () => {
-
-  const form = document.getElementById("loginForm")
-
-  if(!form){
-    console.error("loginForm introuvable")
-    return
-  }
-
-  form.addEventListener("submit", async (e)=>{
-
-    e.preventDefault()
-
-    const email = document.getElementById("email").value.trim()
-    const password = document.getElementById("password").value.trim()
-
-    try{
-
-      const { data, error } = await window.supabaseClient.auth.signInWithPassword({
-        email,
-        password
-      })
-
-      if(error) throw error
-
-      console.log("connecté", data)
-
-      window.location.href = "portal.html"
-
-    }catch(err){
-
-      alert(err.message)
-
-    }
-
-  })
-
-})
+<script src="js/config.js"></script>
+<script src="js/login.js"></script>
