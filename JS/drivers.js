06e2d2bd43236db4c0b6c7e514236be493949e4f
vdiclub-dev <vdiclub@gmail.com>
2026-03-10 18:30:07 +0100

    Update drivers.js

--- JS/drivers.js
+++ JS/drivers.js
@@ -1,1 +1,23 @@
+async function loadDrivers(){
 
+const { data } = await supabase
+.from("drivers")
+.select("*")
+
+data.forEach(driver=>{
+
+L.circleMarker([driver.lat, driver.lng],{
+radius:8,
+color:"green"
+})
+.addTo(map)
+.bindPopup(`
+🚚 ${driver.name}<br>
+Véhicule: ${driver.vehicle}
+`)
+
+})
+
+}
+
+loadDrivers()
