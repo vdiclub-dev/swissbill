f53370b4951c73b278440710bea8d59c74042bc6
vdiclub-dev <vdiclub@gmail.com>
2026-03-11 18:08:27 +0100

    Update config.js

--- dispatch/config.js
+++ dispatch/config.js
@@ -1,1 +1,9 @@
+console.log("config chargé")
 
+const SUPABASE_URL = "https://iubbsnntcreneakbdkmv.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y"
+
+window.supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+)
