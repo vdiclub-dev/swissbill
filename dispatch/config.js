936b038fd2daf57caad4c2705cec2a0c5bba21ff
vdiclub-dev <vdiclub@gmail.com>
2026-03-12 17:45:37 +0100

    Update config.js

--- dispatch/config.js
+++ dispatch/config.js
@@ -6,6 +6,18 @@ window.SUPABASE_CONFIG = {
     url: 'https://iubbsnntcreneakbdkmv.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y'
 };
+// Configuration RH
+window.RH_CONFIG = {
+    heures_nuit_debut: 23,      // 23h
+    heures_nuit_fin: 5,          // 5h
+    majoration_nuit: 0.10,       // +10%
+    majoration_dimanche: 0.50,   // +50%
+    majoration_ferie: 1.00,      // +100%
+    seuil_heures_sup_25: 8,      // 8h
+    seuil_heures_sup_50: 10,     // 10h
+    pause_minimale_apres: 4.5,   // 4h30 de conduite
+    duree_pause: 0.75            // 45 minutes
+};
 
 console.log("✅ Configuration prête");
 
