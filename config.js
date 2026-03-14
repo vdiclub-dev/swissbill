289f0552ee7318e1e861c21a7c8495da91854098
vdiclub-dev <vdiclub@gmail.com>
2026-03-14 18:38:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// ===== CONFIGURATION SUPABASE UNIQUE =====
+// ===== CONFIGURATION SUPABASE =====
 window.SUPABASE_CONFIG = {
     url: 'https://iubbsnntcreneakbdkmv.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1YmJzbm50Y3JlbmVha2Jka212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzI1MDYsImV4cCI6MjA4ODE0ODUwNn0.FzMgCZxNIej1skSIc8UAGiODcZEZW1GCWZwBfonm_1Y'
 };
 
-console.log("✅ Configuration chargée - Prêt à l'emploi");
+console.log("✅ Configuration Supabase chargée");
