a9af1f552230762478c471798d43d3f7a953c172
Didier Gysling <vdiclub@gmail.com>
2026-05-12 07:43:30 +0200

    Fix Panzella pickup kilometer profile

--- config.js
+++ config.js
@@ -33,7 +33,7 @@
 // Quand une mise en production importante a lieu, cette valeur doit évoluer
 // en même temps que les autres marqueurs de version pour éviter les mélanges
 // de vieux HTML et de nouveaux assets.
-window.COLIXO_ASSET_VERSION = '20260511aa';
+window.COLIXO_ASSET_VERSION = '20260511ab';
 
 /**
  * Résout le préfixe d'URL sous lequel le site est servi.
